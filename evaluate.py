1c8cc9728190b2174c353b724a799ef897190580
SJTU-ViSYS <88317801+SJTU-ViSYS@users.noreply.github.com>
2024-07-10 17:11:10 +0800

    eval

--- evaluate.py
+++ evaluate.py
@@ -75,50 +75,50 @@ def validate(model, args, mixed_prec=False, completion_split='val'):
 
             time_count += end - start
 
-    avg_time = time_count / len(val_dataset)
-    avg_fps = 1 / avg_time
+    # avg_time = time_count / len(val_dataset)
+    # avg_fps = 1 / avg_time
 
-    print(f'time: {avg_time}, fps: {avg_fps}')
+    # print(f'time: {avg_time}, fps: {avg_fps}')
 
-    #         depth_pr = depth_pr[-1].unsqueeze(1)
-    #         c_rate = conversion_rate.unsqueeze(1).unsqueeze(1).unsqueeze(1).repeat(1, 1, depth_pr.shape[-2],
-    #                                                                                depth_pr.shape[-1]).cuda()
-    #         flow_pr = c_rate / (depth_pr + 1e-6)
-    #         flow_pr = torch.where(depth_pr == 0, torch.zeros_like(depth_pr, device=depth_pr.device), flow_pr)
-    #         flow_pr = torch.clamp(flow_pr, min=0, max=args.max_disp)
-    #
-    #     flow_pr = padder.unpad(flow_pr).cpu().squeeze(0)
-    #     depth_pr = padder.unpad(depth_pr).cpu().squeeze(0)
-    #
-    #     assert flow_pr.shape == flow_gt.shape, (flow_pr.shape, flow_gt.shape)
-    #
-    #     c_rate = conversion_rate
-    #     conversion_rate = conversion_rate.unsqueeze(1).unsqueeze(1).repeat(1, flow_pr.shape[-2],
-    #                                                                        flow_pr.shape[-1])
-    #     valid_tmp = (valid_gt.unsqueeze(0).bool()) & (
-    #             flow_gt > (c_rate / 100.)) & (
-    #                         flow_gt < args.max_disp) & (flow_gt < (c_rate / depth_range_min)) & (
-    #                         flow_gt > (c_rate / depth_range_max)) & (flow_pr > 0) & (flow_pr > (c_rate / 100.))
-    #     valid_tmp = valid_tmp & (depth_pr > 0)
-    #     depth_est = depth_pr[valid_tmp]
-    #     depth_gt = conversion_rate[valid_tmp] / flow_gt[valid_tmp]
-    #     mae = torch.abs(depth_gt - depth_est).mean()
-    #     rmse = torch.sqrt(F.mse_loss(depth_est, depth_gt))
-    #     imae = torch.abs(
-    #         flow_pr[valid_tmp] / conversion_rate[valid_tmp] - flow_gt[
-    #             valid_tmp] / conversion_rate[valid_tmp]).mean()
-    #     irmse = torch.sqrt(F.mse_loss(flow_pr[valid_tmp] / conversion_rate[valid_tmp],
-    #                                   flow_gt[valid_tmp] / conversion_rate[valid_tmp]))
-    #     mae_sum += mae
-    #     rmse_sum += rmse
-    #     rmse_num += 1
-    #     imae_sum += imae
-    #     irmse_sum += irmse
-    #
-    # mae = mae_sum / (rmse_num + 1e-6)
-    # rmse = rmse_sum / (rmse_num + 1e-6)
-    # imae = imae_sum / (rmse_num + 1e-6)
-    # irmse = irmse_sum / (rmse_num + 1e-6)
-    # print(
-    #     f"mae:{round(mae.item(), 6)}, rmse: {round(rmse.item(), 5)}, imae: {round(imae.item(), 7)}, irmse:{round(irmse.item(), 7)}")
+        depth_pr = depth_pr[-1].unsqueeze(1)
+        c_rate = conversion_rate.unsqueeze(1).unsqueeze(1).unsqueeze(1).repeat(1, 1, depth_pr.shape[-2],
+                                                                               depth_pr.shape[-1]).cuda()
+        flow_pr = c_rate / (depth_pr + 1e-6)
+        flow_pr = torch.where(depth_pr == 0, torch.zeros_like(depth_pr, device=depth_pr.device), flow_pr)
+        flow_pr = torch.clamp(flow_pr, min=0, max=args.max_disp)
+    
+        flow_pr = padder.unpad(flow_pr).cpu().squeeze(0)
+        depth_pr = padder.unpad(depth_pr).cpu().squeeze(0)
+    
+        assert flow_pr.shape == flow_gt.shape, (flow_pr.shape, flow_gt.shape)
+    
+        c_rate = conversion_rate
+        conversion_rate = conversion_rate.unsqueeze(1).unsqueeze(1).repeat(1, flow_pr.shape[-2],
+                                                                           flow_pr.shape[-1])
+        valid_tmp = (valid_gt.unsqueeze(0).bool()) & (
+                flow_gt > (c_rate / 100.)) & (
+                            flow_gt < args.max_disp) & (flow_gt < (c_rate / depth_range_min)) & (
+                            flow_gt > (c_rate / depth_range_max)) & (flow_pr > 0) & (flow_pr > (c_rate / 100.))
+        valid_tmp = valid_tmp & (depth_pr > 0)
+        depth_est = depth_pr[valid_tmp]
+        depth_gt = conversion_rate[valid_tmp] / flow_gt[valid_tmp]
+        mae = torch.abs(depth_gt - depth_est).mean()
+        rmse = torch.sqrt(F.mse_loss(depth_est, depth_gt))
+        imae = torch.abs(
+            flow_pr[valid_tmp] / conversion_rate[valid_tmp] - flow_gt[
+                valid_tmp] / conversion_rate[valid_tmp]).mean()
+        irmse = torch.sqrt(F.mse_loss(flow_pr[valid_tmp] / conversion_rate[valid_tmp],
+                                      flow_gt[valid_tmp] / conversion_rate[valid_tmp]))
+        mae_sum += mae
+        rmse_sum += rmse
+        rmse_num += 1
+        imae_sum += imae
+        irmse_sum += irmse
+    
+    mae = mae_sum / (rmse_num + 1e-6)
+    rmse = rmse_sum / (rmse_num + 1e-6)
+    imae = imae_sum / (rmse_num + 1e-6)
+    irmse = irmse_sum / (rmse_num + 1e-6)
+    print(
+        f"mae:{round(mae.item(), 6)}, rmse: {round(rmse.item(), 5)}, imae: {round(imae.item(), 7)}, irmse:{round(irmse.item(), 7)}")
     return
